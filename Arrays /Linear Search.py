c7a51d2f2afd959c15f560678a64f590996b5ac1
Kriti Priya <105275392+11kriti@users.noreply.github.com>
2025-06-14 18:37:26 +0530

    Update Linear Search.py

--- Arrays /Linear Search.py
+++ Arrays /Linear Search.py
@@ -1,1 +1,16 @@
+#Linear Search 
+#Input: arr = 2,4,1,5,7 , key = 1 , output = index of the number = 2
+
+def linear_search(arr , size , key):
+    
+    size = len(arr)
+    for i in range(size):
+        if key == arr[i]:
+            return i
+    return -1
+    
+arr = [2,4,1,5,7]
+key = 8
+size = len(arr)
+print("Linear Search -" , linear_search(arr,size, key))
 
