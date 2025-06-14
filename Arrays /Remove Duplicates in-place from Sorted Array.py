278587f6daf04aeb6e4b197f2d2e2c4c2791edb6
Kriti Priya <105275392+11kriti@users.noreply.github.com>
2025-06-14 18:19:44 +0530

    Update Remove Duplicates in-place from Sorted Array.py

--- Arrays /Remove Duplicates in-place from Sorted Array.py
+++ Arrays /Remove Duplicates in-place from Sorted Array.py
@@ -1,1 +1,21 @@
+#Remove Duplicates in-place from Sorted Array
+#Input: arr[1,1,2,2,2,3,3] ,Output: arr[1,2,3,_,_,_,_]
 
+def removeDuplicates (arr: list[int]) ->int:
+    st = set()
+    n = len(arr)
+    for i in range(n):
+        st.add(arr[i])
+    store = len(st)
+    j = 0 
+    for x in st:
+        arr[j] = x
+        j+=1
+    return store
+    
+arr = [1, 1, 2, 2, 2, 3, 3]
+k = removeDuplicates(arr)
+print("The array after removing duplicate elements is ")
+for i in range(k):
+    print(arr[i], end=" ")
+    
