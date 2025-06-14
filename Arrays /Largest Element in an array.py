0336e8b2da527941c8ef0b01660fff6f7d77b9ca
Kriti Priya <105275392+11kriti@users.noreply.github.com>
2025-06-14 17:13:48 +0530

    Update Largest Element in an array.py

--- Arrays /Largest Element in an array.py
+++ Arrays /Largest Element in an array.py
@@ -2,10 +2,16 @@
 ''' Input - arr = 10,2,4,6,7
 output = 10'''
 
-#through sorting
+#through comparing and marking MAX as 0th index at first
 def largest_elem(arr , size):
     max = arr[0]
     for i in range( 0 , size):
         if arr[i] > max:
             max = arr[i]
     return max
+
+#through sorting
+def largest(arr,size):
+    large = arr.sort()
+    return large
+
