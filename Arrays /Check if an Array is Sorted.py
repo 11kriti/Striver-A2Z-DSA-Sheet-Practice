238e0b33590a391217c325f5c84361ea90ff1d7a
Kriti Priya <105275392+11kriti@users.noreply.github.com>
2025-06-14 17:38:04 +0530

    Update Check if an Array is Sorted.py

--- Arrays /Check if an Array is Sorted.py
+++ Arrays /Check if an Array is Sorted.py
@@ -10,3 +10,10 @@ def sorted_or_not(arr,size):
 arr = [1,3,5,7,9]
 size = 5
 print("True" if sorted_or_not(arr, size) else "False")
+
+#in O(N)
+def sorted_not(arr, n):
+    for i in range(1, n):
+        if arr[i] < arr[i - 1]:
+            return False
+    return True
