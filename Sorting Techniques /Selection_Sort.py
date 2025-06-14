57699875c19f44ab29af0067acdde6ade1604c38
Kriti Priya <105275392+11kriti@users.noreply.github.com>
2025-06-14 15:47:26 +0530

    Update Selection_Sort.py

--- Sorting Techniques /Selection_Sort.py
+++ Sorting Techniques /Selection_Sort.py
@@ -9,6 +9,6 @@ def sorting_tech(arr):
   for i in range(n-1):
     min_index = i
     for j in range(i+1 , n):
-      if arr[j] >< arr[min_index]:
+      if arr[j] < arr[min_index]:
         min_index = j
     arr[j] , arr[min_index] = arr[min_index] , arr[j]
