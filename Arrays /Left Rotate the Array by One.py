0b2b8c59943a94a7a64c2d71b5f5f294e272b07d
Kriti Priya <105275392+11kriti@users.noreply.github.com>
2025-06-15 11:22:29 +0530

    Update Left Rotate the Array by One.py

--- Arrays /Left Rotate the Array by One.py
+++ Arrays /Left Rotate the Array by One.py
@@ -1,1 +1,13 @@
-
+#Left Rotate the Array by One
+#input : arr = [1, 2, 3, 4, 5] , Output : 2 3 4 5 1
+def rotate_arr_by_one(arr, size):
+    temp_arr = [0]*size
+    for i in range(1,size):
+        temp_arr[i-1] = arr[i]
+    temp_arr[size-1] = arr[0]
+    for i in range(size):
+        print(temp_arr[i], end=" ")
+    print()
+arr = [1,2,3,4,5]
+size = 5
+(rotate_arr_by_one(arr,size))
