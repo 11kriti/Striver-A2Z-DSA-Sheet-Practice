0d3ff1709c10a5ffb377ea23f0be80c94019e9f8
Kriti Priya <105275392+11kriti@users.noreply.github.com>
2025-06-16 20:16:00 +0530

    Update Binary Search code.py

--- Binary Search/Binary Search code.py
+++ Binary Search/Binary Search code.py
@@ -1,1 +1,28 @@
 
+#binary search
+def binary_search(arr, key):
+    size = len(arr)
+    start = 0
+    end = size - 1
+    
+    
+    while start <= end:
+        mid = (start+end) // 2
+        if arr[mid ]== key:
+            return mid
+        elif key > arr[mid]:
+            start = mid + 1
+        else:
+            end = mid -1 
+    return -1
+    
+   
+
+a = [3, 4, 6, 7, 9, 12, 16, 17]
+target = 6
+size = len(a)
+ind = binary_search(a, target)
+if ind == -1:
+    print("The target is not present.")
+else:
+    print("The target is at index:", ind)
