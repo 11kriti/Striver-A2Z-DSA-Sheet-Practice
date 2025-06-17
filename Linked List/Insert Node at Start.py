d5898a403bde2fa1cc1cbacba369ad5c628ea43c
Kriti Priya <105275392+11kriti@users.noreply.github.com>
2025-06-17 13:47:17 +0530

    Update Insert Node at Start.py

--- Linked List/Insert Node at Start.py
+++ Linked List/Insert Node at Start.py
@@ -1,1 +1,31 @@
+#inked list - to make a node 
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+#insert a node at the starting of LL
+def insert_at_start(head , new_data):
+    new_data = Node(new_data)
+    new_data.next = head
+    head = new_data
+    return head
+        
+#print fun for linkedlist
+def printLL(head):
+    temp = head
+    while temp:
+        print(temp.data , end = "->")
+        temp = temp.next
+    print("None")
+    
+node1 = Node(5)
+node2 = Node(3)
+node3 = Node(24)
 
+node1.next = node2
+node2.next = node3
+head = node1
+
+head = insert_at_start(head, 11)
+
+printLL(head)
