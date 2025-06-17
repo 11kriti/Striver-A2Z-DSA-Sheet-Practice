5081f0c46e50b63b97def2beaddcff2a54188302
Kriti Priya <105275392+11kriti@users.noreply.github.com>
2025-06-17 16:33:37 +0530

    Create Insert at End in DLL.py

---
+++ Linked List/Insert at End in DLL.py
@@ -0,0 +1,47 @@
+#create a node for DLL
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self .prev = None
+        self.next = None
+        
+#insert node at starting 
+def insert_node_at_start(head , new_data):
+    new_node = Node(new_data)
+    new_node.next = head
+    if head is not None:
+        head.prev = new_node
+    return new_node
+#inseert at end 
+def insert_at_end(head, new_data):
+    new_node = Node(new_data)
+    if head is None:
+        return new_node
+    temp = head
+    while temp.next is not None:
+        temp = temp.next
+    temp.next = new_node
+    new_node.prev = temp
+    return head
+#print a DLL
+def printDLL(head):
+    temp = head
+    while temp:
+        print(temp.data , end = "<->")
+        temp = temp.next
+    print("None")
+    
+node1 = Node(24)
+node2 = Node(11)
+node3 = Node(25)
+
+node1.next = node2
+node2.prev = node1
+node2.next = node3
+node3.prev = node2
+head = node1
+
+head = insert_node_at_start(head , 1)
+head = insert_at_end(head, 111)
+
+printDLL(head)
