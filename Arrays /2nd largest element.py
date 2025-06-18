717227e72a8efe09155a17bed4b6584356b43884
Kriti Priya <105275392+11kriti@users.noreply.github.com>
2025-06-18 16:11:29 +0530

    Create 2nd largest element.py

---
+++ Arrays /2nd largest element.py
@@ -0,0 +1,9 @@
+def second_largest(arr ,size):
+    arr.sort()
+    return arr[size-2]
+    
+arr = [1,2,3,5,6]
+largest = second_largest(arr,5)
+#sorteds = is_sorted_or_not(arr,5)
+#large = largest_elem(arr,5)
+print(largest)
