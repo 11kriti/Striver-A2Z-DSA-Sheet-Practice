7779271bf7bc4dea654eca7d11aa6f3c81b03ded
Kriti Priya <105275392+11kriti@users.noreply.github.com>
2025-06-19 20:45:30 +0530

    Create #Last occurrence in a sorted array.py

---
+++ Binary Search/#Last occurrence in a sorted array.py
@@ -0,0 +1,16 @@
+#Last occurrence in a sorted array
+#Input: N = 7, target=13, array[] = {3,4,13,13,13,20,40} , Output: 4
+# through linear search
+def last_occur(arr , target):
+    size = len(arr)
+    res = -1
+    for i in range(size-1 , -1 , -1):
+        if arr[i] == target:
+            res = i
+            break
+    return res
+    
+arr = [3,4,13,13,13,20,40]
+size = len(arr)
+occurence = last_occur(arr , 13)
+print(occurence)
