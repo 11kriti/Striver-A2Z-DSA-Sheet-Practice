722e4f55f5a71b1828244157195e053b4f100331
Kriti Priya <105275392+11kriti@users.noreply.github.com>
2025-06-19 20:27:59 +0530

    Update Implement Lower Bound.py

--- Binary Search/Implement Lower Bound.py
+++ Binary Search/Implement Lower Bound.py
@@ -5,8 +5,26 @@ def lower_bound(arr , key):
     if arr[i] >= key:
       return i
   return size
+
+#through binary search
+
+def lower_bound_BS(arr , key):
+  size = len(arr)
+  start = 0 
+  end = size -1 
+  result = size
+  
+  while start <= end :
+    mid = (start +end) // 2
+    if arr[mid] >= key:
+      result = mid
+   
+      end = mid -1
+    else:
+      start = mid+1
+  return result
   
 arr = [11,24,25,78,89]
 size = len(arr)
-lower = lower_bound(arr, 26)
+lower = lower_bound_BS(arr, 26)
 print(lower)
