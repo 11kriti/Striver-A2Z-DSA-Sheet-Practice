36db8b8ac3dce9ca493f9925ab6d3ad3a21f7815
Kriti Priya <105275392+11kriti@users.noreply.github.com>
2025-06-23 23:48:52 +0530

    Update #Count Occurrences in Sorted Array.py

--- Binary Search/#Count Occurrences in Sorted Array.py
+++ Binary Search/#Count Occurrences in Sorted Array.py
@@ -1,4 +1,4 @@
-#Count Occurrences in Sorted Array
+####Count Occurrences in Sorted Array
 #Input: N = 7,  X = 3 , array[] = {2, 2 , 3 , 3 , 3 , 3 , 4}  Output: 4
 #linear search
 def count_occur(arr , X):
